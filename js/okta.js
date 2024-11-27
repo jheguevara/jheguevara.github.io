906ec904de5b8da9770c8f8268a957a1f2680473
Amanpreet <aman.xennial@gmail.com>
2024-11-27 09:23:37 -0500

    Allow a logged in user to return to the index.html page

--- js/okta.js
+++ js/okta.js
@@ -91,8 +91,24 @@ window.onload = async () => {
       setCookie("user", JSON.stringify(user), 7)
       sessionStorage.setItem('user', JSON.stringify(user))
       console.log('USER:  ' + JSON.stringify(user.name));
-      window.location.replace('/pages/apps.html')
+      //   window.location.replace('/pages/apps.html')
+      const isAdmin = user.role === 'admin';
 
+      console.log("Checking to see if admin " + isAdmin);
+      eachElement(".profile-image", (e) => (e.src = user.picture));
+      eachElement(".auth-invisible", (e) => e.classList.add("hidden"));
+      eachElement(".auth-visible", (e) => e.classList.remove("hidden"));
+
+      if (isAdmin) {
+        console.log("User is admin");
+
+        eachElement(".admin-invisible", (e) => e.classList.add("hidden"));
+        eachElement(".admin-visible", (e) => e.classList.remove("hidden"));
+
+      } else {
+        console.log("Run this if the user is authenticated but not admin.");
+        displayNonAdminFields();
+      }
       return;
     } else {
       document.querySelectorAll('.auth-invisible').forEach(field => {
